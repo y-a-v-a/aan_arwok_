69a2eb89bbb25599adc39a3f673331a4f69bf317
Vincent Bruijn <vebruijn@gmail.com>
2019-07-10 14:48:26 +0200

    layout updates

--- routes/index.js
+++ routes/index.js
@@ -9,25 +9,50 @@ var localizeOptions = {
 
 var title = 'On Kawara font';
 
+function getTheme() {
+  var options = [
+    'white-on-gray',
+    'white-on-red',
+    'white-on-greenish'
+  ];
+
+  var randomIndex = Math.floor(Math.random() * 3);
+  return options[randomIndex];
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var today = (new Date()).toLocaleDateString('en-US', localizeOptions);
+  var theme = getTheme();
 
   const responseData = {
     title: title,
-    date: today
+    date: today,
+    theme: theme
   };
 
   res.render('index', responseData);
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: `${title} about` });
+  var theme = getTheme();
+  res.render('about', {
+    title: `${title} about`,
+    theme: theme
+  });
 });
 
 router.get('/download', function(req, res, next) {
-  res.render('download', { title: `${title} download` });
+  var theme = getTheme();
+  res.render('download', {
+    title: `${title} download`,
+    theme: theme
+  });
 });
 
 
 module.exports = router;
+
+
+// on kawara
+// aaaknorw
